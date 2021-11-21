37a6df3c5f9eb13218c55ec22c6baf6e330a7e99
jakkie <jakubkielbasiewicz@gmail.com>
2021-11-21 22:58:59 +0000

    initial commit

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -76,6 +76,7 @@ long LinuxParser::UpTime() { return 0; }
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
 
+
 // TODO: Read and return the number of active jiffies for a PID
 // REMOVE: [[maybe_unused]] once you define the function
 long LinuxParser::ActiveJiffies(int pid[[maybe_unused]]) { return 0; }
