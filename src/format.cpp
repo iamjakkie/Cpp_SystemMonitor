38e740dd00bb5309c942b2ce16559991e0fd35cc
jakkie <jakubkielbasiewicz@gmail.com>
2021-11-22 22:25:03 +0000

    Time formatter

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,12 @@
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    string h = std::to_string(seconds / 3600);
+    string m = std::to_string((seconds%=3600) / 60);
+    string s = std::to_string((seconds%=3600)%=60);
+    string hh = string(2 - h.length(), '0') + h;
+    string mm = string(2 - m.length(), '0') + m;
+    string ss = string(2 - s.length(), '0') + s;
+    return hh+':'+mm+':'+ss;
+}
